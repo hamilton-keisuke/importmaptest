2f413f4e0ac11a2ab418f4f7312b9c8bac047367
hamilton-keisuke <hamiltonkeisuke@gmail.com>
2021-10-22 12:55:58 +0900

    sample using list.js by importmap
    command:
      bin/importmap pin list.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import List from "list.js"
+
+const listjsOption = {
+  valueNames: [ 'title', 'content' ]
+}
+
+const postsList = new List('posts', listjsOption)
